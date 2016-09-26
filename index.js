bace134d565af25f2186f4c90250d684e0452691
Jared Hirsch <ohai@6a68.net>
2016-09-26 16:10:24 -0700

    Switch to a vanilla SDK panel
    Workaround for issue #307.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
  * http://mozilla.org/MPL/2.0/.
  */
 
+const { getActiveView } = require('sdk/view/core');
 const pageMod = require('sdk/page-mod');
 
 const getYouTubeUrl = require('./lib/get-youtube-url');
@@ -11,19 +12,89 @@ const getVimeoUrl = require('./lib/get-vimeo-url');
 const launchVideo = require('./lib/launch-video');
 const sendMetricsData = require('./lib/send-metrics-data');
 const contextMenuHandlers = require('./lib/context-menu-handlers');
-const panelUtils = require('./lib/panel-utils');
 
-let browserResizeMod, launchIconsMod;
+let browserResizeMod, launchIconsMod, panel;
+
+const dimensions = {
+  height: 180,
+  minimizedHeight: 40,
+  width: 320
+}
 
 exports.main = function() {
+  panel = require('sdk/panel').Panel({
+    contentURL: './default.html',
+    contentScriptFile: './controls.js',
+    width: 320,
+    height: 180,
+    position: {
+      bottom: 10,
+      left: 10
+    }
+  });
+
+  panel.port.on('addon-message', opts => {
+    const title = opts.action;
+
+    if (title === 'send-to-tab') {
+      const pageUrl = getPageUrl(opts.domain, opts.id, opts.time);
+      if (pageUrl) require('sdk/tabs').open(pageUrl);
+      else {
+        console.error('could not parse page url for ', opts); // eslint-disable-line no-console
+        panel.port.emit('set-video', {error: 'Error loading video from ' + opts.domain});
+      }
+      panel.port.emit('set-video', {domain: '', src: ''});
+      panel.hide();
+    } else if (title === 'close') {
+      panel.port.emit('set-video', {domain: '', src: ''});
+      panel.hide();
+    } else if (title === 'minimize') {
+      adjustHeight(dimensions.minimizedHeight);
+    } else if (title === 'maximize') {
+      adjustHeight(dimensions.height);
+    } else if (title === 'metrics-event') {
+      sendMetricsData(opts, panel);
+    }
+  });
+
+  function getPageUrl(domain, id, time) {
+    let url;
+    if (domain.indexOf('youtube') > -1) {
+      url = `https://youtube.com/watch?v=${id}&t=${Math.floor(time)}`;
+    } else if (domain.indexOf('vimeo') > -1) {
+      const min = Math.floor(time / 60);
+      const sec = Math.floor(time - min * 60);
+      url = `https://vimeo.com/${id}#t=${min}m${sec}s`;
+    } else if (domain.indexOf('vine') > -1) {
+      url = `https://vine.co/v/${id}`;
+    }
+
+    return url;
+  }
+
+  // newHeight should be just a number, like 40.
+  function adjustHeight(newHeight) {
+    const xulPanel = getActiveView(panel);
+    const frames = xulPanel.getElementsByTagName('iframe');
+    const frame = frames.length && frames[0];
+    if (!frame) return console.error('Unable to change panel height: iframe not found'); // eslint-disable-line no-console
+    frame.style.height = newHeight + 'px';
+    xulPanel.moveToAnchor(xulPanel.ownerDocument.documentElement, 'bottomleft bottomleft', 10, -10);
+  }
+
+  // Keep the panel open when it loses focus.
+  getActiveView(panel).setAttribute('noautohide', true);
+
   // handle browser resizing
   browserResizeMod = pageMod.PageMod({
     include: '*',
     contentScriptFile: './resize-listener.js?cachebust=' + Date.now(),
     onAttach: function(worker) {
       worker.port.on('resized', function() {
-        const panel = panelUtils.getPanel();
-        if (panel && panel.isShowing) panelUtils.redraw();
+        if (panel && panel.isShowing) {
+          const xulPanel = getActiveView(panel);
+          xulPanel.moveToAnchor(xulPanel.ownerDocument.documentElement, 'bottomleft bottomleft', 10, -10);
+        }
       });
     }
   });
@@ -41,26 +112,26 @@ exports.main = function() {
             object: 'overlay_icon',
             method: 'launch',
             domain: opts.domain
-          });
-          launchVideo(opts);
+          }, panel);
+          launchVideo(opts, panel);
         } else if (opts.domain.indexOf('vimeo.com')  > -1) {
           opts.getUrlFn = getVimeoUrl;
           sendMetricsData({
             object: 'overlay_icon',
             method: 'launch',
             domain: opts.domain
-          });
-          launchVideo(opts);
+          }, panel);
+          launchVideo(opts, panel);
         }
       });
       worker.port.on('metrics', sendMetricsData);
     }
   });
 
-  contextMenuHandlers.init(panelUtils.getPanel());
+  contextMenuHandlers.init(panel);
 };
 exports.onUnload = function(reason) {
-  panelUtils.destroy();
+  panel.destroy();
   contextMenuHandlers.destroy();
   browserResizeMod.destroy();
   launchIconsMod.destroy();
