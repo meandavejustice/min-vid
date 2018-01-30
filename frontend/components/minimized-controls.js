7e0777fb1add257f814de4f9aa79f329b0f2bc89
Dave Justice <djustice@mozilla.com>
2018-01-30 20:34:45 +0000

    remove next and previous buttons from minimized view (#1230)
    - fixes #1214

--- frontend/components/minimized-controls.js
+++ frontend/components/minimized-controls.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import PrevTrackBtn from './prev-button';
-import NextTrackBtn from './next-button';
 import SoundControl from './sound-control';
 import GeneralControls from './general-controls';
 import PlaybackControl from './playback-control';
@@ -15,9 +13,7 @@ export default class MinimizedControls extends React.Component {
         <div className='bottom-controls controls minimized'>
             <Progress {...this.props} />
             <div className='left'>
-              <PrevTrackBtn {...this.props} />
               <PlaybackControl {...this.props} />
-              <NextTrackBtn {...this.props} />
             </div>
             <div className='right'>
               <SoundControl {...this.props} />
